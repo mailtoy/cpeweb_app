d70102eaf920aecd609ee23a92a06ffe5a3b03ad
Non Puthikanon <non.pu@ku.th>
2017-05-25 08:34:22 +0000

    about css

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,6 @@
 class StaticPagesController < ApplicationController
-<<<<<<< HEAD
   def about
-=======
+  end
   def contact
->>>>>>> 935529af7b76d33e24a23fa4ce9a61a9c3823c0e
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'static_pages/about'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
-
-  get 'static_pages/contact'
-
-root 'application#home'
-
->>>>>>> 935529af7b76d33e24a23fa4ce9a61a9c3823c0e
 end
-
-
