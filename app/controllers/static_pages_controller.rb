277098cbc3f33cbcba1c4cbe2e7ee122575af29d
Dacharat Pankong <dacharat.p@ku.th>
2017-05-25 10:49:59 +0000

    Add reseach

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
   def admini
   end
+  def reseach
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get 'static_pages/admini'
+  
+  get 'static_pages/reseach'
 end
