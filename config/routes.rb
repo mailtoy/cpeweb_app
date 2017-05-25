a0ed88c1a46777c70c06055b767022b02ad8ba7e
Triwith Mutitakul <triwith.m@ku.th>
2017-05-25 07:31:46 +0000

    Merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'static_pages#Home'
+
+get 'static_pages/Home'
+
+# get 'static_pages/help'
+
+# get 'static_pages/about'
+
 end
