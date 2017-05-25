fbec898687fb7e9e8dbe214faa8f0b5e9f35044a
Triwith Mutitakul <triwith.m@ku.th>
2017-05-25 09:53:03 +0000

    Merge with Jack

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,6 @@ class StaticPagesController < ApplicationController
 
   def about
   end
-
   def contact
   end
   def home
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
+    root 'static_pages#home'
 
-get 'static_pages/home'
+ get '/about', to: 'static_pages#about'
 
-  get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
 
-  root 'application#home'
 end
